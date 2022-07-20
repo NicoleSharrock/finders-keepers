c858fabda31d2cc5b439b7da4cd551dc2ff3ae50
Allyson Nasworthy <allyson.nasworthy@gmail.com>
2022-07-19 20:18:48 -0400

    test hompage renders

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello!</h1>
-      </header>
-    </div>
+    < Home />
   );
 }
 
