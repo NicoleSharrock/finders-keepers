df23ebb3892ec628903ba3973690bb00e74f7942
MadStoddard <madelinejdierking@gmail.com>
2022-07-21 19:54:05 -0400

    rough footer CSS needed

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -6,16 +6,23 @@ const Gallery = () => {
         //     <h1>This is the Gallery Page.</h1>
         // </main>
         <main>
-            <h1 class="title">Finder's Keepers Gallery</h1>
-            <nav>
                 <ul>
                     <a href="">
                         <li><button class="b1">Add New Item</button></li>
                     </a>
                     <li><button class="b2">Log Out</button></li>
                 </ul>
-            </nav>
              {/* <div id="gallery-wall"></div> */}
+
+             <footer>
+                <p>Click on each name to see more work from the contributors!</p>
+                <p class="names">
+                    <a href="https://github.com/NicoleSharrock">Nicole Sharrock-Hayes</a> 
+                    <a href="https://github.com/Awnasworthy">Ally Nasworthy</a>
+                    <a href="https://github.com/Emilyreddy">Emily Reddy</a>
+                    <a href="https://github.com/MadStoddard">Madeline Stoddard</a>
+                </p>
+            </footer>
         </main>
     )
 }
