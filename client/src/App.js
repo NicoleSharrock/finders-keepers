3bfb20fd7fe6c7019ec7175a17aa89d386e8deff
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-07-21 20:53:13 -0400

    working on nav btwn pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import Gallery from './pages/Gallery';
 import Add from './pages/Add';
 import Header from './components/Header'
 import Nav from './components/Nav';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+// import { Route, Link } from 'react-router-dom';
 
 function App() {
   return (
