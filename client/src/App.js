bafd1bcd1e39d740348b36a3e779fc278d722187
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-07-27 17:27:31 -0400

    upated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import Login from './components/Login';
 import Gallery from './pages/Gallery';
 import Add from './pages/Add';
